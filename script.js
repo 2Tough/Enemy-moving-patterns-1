0bf80b2cfd3fdd4f4a4182ae661f112d7152f2c0
2Tough <vainferro@gmail.com>
2022-12-03 16:43:59 +0300

    Debugged and got flying bats.

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 /** @type {HTMLCanvasElement} */
+
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100
-const enemiesArray = [];
-
-const enemyImage = new Image();
-enemyImage.src = './images/enemy1.png'
+const numberOfEnemies = 1000
+const enemiesArray = []
+let gameFrame = 0;
 
 class Enemy {
     constructor(){
+        this.image = new Image();
+        this.image.src = './images/enemy1.png';
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+
+        this.speed = Math.random() * 4 - 2
         this.spriteWidth = 293;
-        this.spriteHeigth = 155;
-        this.width = this.spriteWidth  / 2.5;
-        this.height = this.spriteHeight  / 2.5;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x += this.speed
-        this.y += this.speed
+        this.x += this.speed;
+        this.y += this.speed;
+        if(gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeigth, this.x, this.y, this.width, this.height)
+        
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 };
 
@@ -35,11 +42,13 @@ for (let i = 0; i < numberOfEnemies ; i++) {
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    enemiesArray.forEach( enemy => {
+    enemiesArray.forEach(enemy=> {
         enemy.update()
         enemy.draw()
         
+        
     });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
