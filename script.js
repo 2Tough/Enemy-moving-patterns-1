e69b16103fd9aaff1355b4525912da04283d3cee
2Tough <vainferro@gmail.com>
2022-12-03 15:40:58 +0300

    Added random sprite movement.

---
+++ script.js
@@ -0,0 +1,45 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100
+const enemiesArray = [];
+
+const enemyImage = new Image();
+enemyImage.src = './images/enemy1.png'
+
+class Enemy {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeigth = 155;
+        this.width = this.spriteWidth  / 2.5;
+        this.height = this.spriteHeight  / 2.5;
+    }
+    update() {
+        this.x += this.speed
+        this.y += this.speed
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeigth, this.x, this.y, this.width, this.height)
+    }
+};
+
+for (let i = 0; i < numberOfEnemies ; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    enemiesArray.forEach( enemy => {
+        enemy.update()
+        enemy.draw()
+        
+    });
+    requestAnimationFrame(animate);
+}
+animate();
